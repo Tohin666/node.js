456ae415ef4bf2cad16d2aa44c182549dba9e936
tohin666 <tohin666@gmail.com>
2020-02-07 07:56:40 +0300

    lesson1

--- index.js
+++ index.js
@@ -8,18 +8,6 @@ cursor
 .white()
 .bg.green()
 .write('Hello world!')
-.bg.reset()
-.write('\n')
-.blue()
-.bg.white()
-.write('This is task1 of lesson1!')
 .reset()
 .bg.reset()
 .write('\n');
-
-var colors = require('colors');
- 
-console.log('This'.green);
-console.log('is'.underline.red);
-console.log('task2'.inverse);
-console.log('of lesson1!'.rainbow);
