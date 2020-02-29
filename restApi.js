3005ad253b0f06756df3ec6471ccbf222afb743e
tohin666 <tohin666@gmail.com>
2020-02-29 17:39:11 +0300

    lesson8 complete

--- restApi.js
+++ restApi.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const request = require('request');
+const cheerio = require('cheerio');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 //Connect
 mongoose.connect('mongodb://localhost:27017/tasks', {
@@ -11,9 +16,71 @@ mongoose.connect('mongodb://localhost:27017/tasks', {
 
 const taskMongoose = require('./models/task');
 const userMongoose = require('./models/user');
+const newsMongoose = require('./models/news');
 const passport = require('./passport');
 
 const app = express();
+
+//Create a server
+const server = http.Server(app);
+const io = socketIO(server);
+
+io.use((socket, next) => {
+    const token = socket.handshake.query.token;
+
+    jwt.verify(token, 'Very secret code', (err) => {
+        if (err) {
+            return next(new Error('Token not valid'));
+        }
+
+        next();
+    });
+    return next(new Error('Token not valid'));
+});
+
+io.on('connection', (socket) => {
+    console.log('New connection!');
+
+    socket.on('create', async (data) => {
+        const task = new taskMongoose(data);
+        const saved = await task.save();
+
+        socket.broadcast.emit(`created:${saved.user}`, saved);
+        socket.emit(`created:${saved.user}`, saved);
+    });
+
+    socket.on('toggle', async (taskId) => {
+        const task = await taskMongoose.findById(taskId);
+        await taskMongoose.findOneAndUpdate({ _id: taskId }, { $set: { completed: !task.completed } });
+
+        socket.broadcast.emit(`toggled:${task.user}`, taskId);
+        socket.emit(`toggled:${task.user}`, taskId);
+    });
+
+
+    socket.on('getNews', () => {
+        getTodayInCinema().then(
+            (news) => {
+                news.forEach(async (item) => {
+                    const newsItem = new newsMongoose(item);
+                    await newsItem.save();
+                });
+                socket.broadcast.emit('Received', news);
+                socket.emit('Received', news);
+            },
+            (err) => {
+                console.log('Error:', err);
+            }
+        );
+    });
+
+
+    socket.on('disconnect', () => {
+        console.log('Someone disconnect');
+    });
+});
+
+
 app.use(express.json());
 
 app.use(cors());
@@ -22,18 +89,18 @@ app.use(cors());
 const checkAuth = (req, res, next) => {
     //Bearer <token>
 
-    if(req.headers.authorization){
+    if (req.headers.authorization) {
         const [type, token] = req.headers.authorization.split(' ');
 
         //Валидация токена
-         jwt.verify(token, 'Very secret code', (err, decoded) => {
-             if(err){
-                 return res.status(403).send();
-             }
+        jwt.verify(token, 'Very secret code', (err, decoded) => {
+            if (err) {
+                return res.status(403).send();
+            }
 
-             req.user = decoded;
-             next();
-         });
+            req.user = decoded;
+            next();
+        });
     } else {
         return res.status(403).send();
     }
@@ -43,8 +110,8 @@ app.use('/tasks', checkAuth);
 
 app.get('/tasks', async (req, res) => {
     //Pages
-    const {page = 1, limit = 10} = req.query;
-    const tasks = await taskMongoose.find({user: req.user._id}).skip((page - 1) * limit).limit(limit);
+    const { page = 1, limit = 10 } = req.query;
+    const tasks = await taskMongoose.find({ user: req.user._id }).skip((page - 1) * limit).limit(limit);
     res.status(200).json(tasks);
 });
 
@@ -54,69 +121,41 @@ app.get('/tasks/:id', async (req, res) => {
 });
 
 app.post('/tasks', async (req, res) => {
-    const task = new taskMongoose({...req.body, user: req.user._id});
+    const task = new taskMongoose({ ...req.body, user: req.user._id });
 
     task.save()
-    .then((saved) => {
-        res.status(204).json(saved);
-    })
-    .catch(() => {
-        res.status(400).json({message: 'Task dont saved'});
-    });
-});
-
-
-app.patch('/tasks', async (req, res) => {
-    const { id, ...rest} = req.body;
-
-    taskMongoose.updateOne({ _id: id }, rest, function (err, res) {
-        if (err) console.log(handleError(err));
-    })    
-    .then((saved) => {
-        res.status(204).json(saved);
-    })
-    .catch(() => {
-        res.status(400).json({message: 'Task dont saved'});
-    });
-});
-
-app.delete('/tasks/:id', async (req, res) => {    
-
-    taskMongoose.deleteOne({ _id: req.params.id }, function (err, res) {
-        if (err) console.log(handleError(err));
-    })    
-    .then(() => {
-        res.status(204).json({message: 'Task deleted'});
-    })
-    .catch(() => {
-        res.status(400).json({message: 'Task dont deleted'});
-    });
+        .then((saved) => {
+            res.status(204).json(saved);
+        })
+        .catch(() => {
+            res.status(400).json({ message: 'Task dont saved' });
+        });
 });
 
 
 //Register
 app.post('/register', async (req, res) => {
-    const {repassword, ...restBody} = req.body;
-    if(restBody.password === repassword){
+    const { repassword, ...restBody } = req.body;
+    if (restBody.password === repassword) {
         const user = new userMongoose(restBody);
         await user.save();
         return res.status(201).send();
     } else {
-        res.status(400).json({messageError: 'Error registration!'});
+        res.status(400).json({ messageError: 'Error registration!' });
     }
 });
 
 //Auth
 app.post('/auth', async (req, res) => {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
 
-    const user = await userMongoose.findOne({email: username});
+    const user = await userMongoose.findOne({ email: username });
 
-    if(!user){
+    if (!user) {
         return res.status(401).send();
     }
 
-    if(!user.validatePassword(password)){
+    if (!user.validatePassword(password)) {
         return res.status(401).send();
     }
 
@@ -129,25 +168,46 @@ app.post('/auth', async (req, res) => {
     });
 });
 
-app.listen(4000, () => {
-    console.log('Server works!');
+//Для Frontend
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'));
+});
+
+app.get('/auth', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'auth.html'));
 });
 
 
-/*
+function getTodayInCinema() {
+    return new Promise(function (resolve, reject) {
 
-POST, http://localhost:4000/tasks
-{
-	"title":"New task",
-    "completed": "false"
+        const todayInCinema = [];
+
+        request('https://www.kinopoisk.ru/', (err, response, body) => {
+
+            if (!err && response.statusCode === 200) {
+
+                const $ = cheerio.load(body);
+
+                $('.today-in-cinema .carousel__inner').children().each((idx, element) => {
+
+                    if ($(element).find('.film-poster-snippet-partial-component__title').text()) {
+
+                        todayInCinema.push({
+                            title: $(element).find('.film-poster-snippet-partial-component__title').text(),
+                            description: $(element).find('.film-poster-snippet-partial-component__caption').text()
+                        });
+                    }
+                });
+            }
+
+            resolve(todayInCinema);
+            reject(new Error('Error'));
+        });
+    });
 }
 
-PATCH, http://localhost:4000/tasks
-{
-	"id": "5e5806f7f0f92f2101a8dc69",
-    "completed": true
-}
 
-DELETE, http://localhost:4000/tasks/id
-
-*/
+server.listen(4000, () => {
+    console.log('Server works!');
+});
